2484be93057406273172d8fff48eb49da52edb90
Fly_SunShine_J <857402355@qq.com>
2017-04-12 17:24:57 +0800

    新增资源文件

---
+++ 喜马拉雅FM/喜马拉雅FM/FMClass/FMBaseViewController/BaseNavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  BaseNavigationController.swift
+//  喜马拉雅FM
+//
+//  Created by vcyber on 17/4/11.
+//  Copyright © 2017年 vcyber. All rights reserved.
+//
+
+import UIKit
+
+class BaseNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 喜马拉雅FM/喜马拉雅FM/FMHelper/ApplyService.swift
@@ -0,0 +1,13 @@
+//
+//  ApplyService.swift
+//  喜马拉雅FM
+//
+//  Created by vcyber on 17/4/11.
+//  Copyright © 2017年 vcyber. All rights reserved.
+//
+
+import UIKit
+
+class ApplyService: NSObject {
+
+}
